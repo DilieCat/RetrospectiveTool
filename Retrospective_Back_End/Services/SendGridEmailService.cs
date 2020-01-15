79a3a3254ec7d1970afdefa80eefd106a7dd9a78
Brion <khobryan2000@gmail.com>
2020-01-15 10:48:36 +0100

    Added service for Sendgrid emailing

--- Retrospective_Back_End/Services/SendGridEmailService.cs
+++ Retrospective_Back_End/Services/SendGridEmailService.cs
@@ -7,17 +7,24 @@ namespace Retrospective_Back_End.Services
 {
     public static class SendGridEmailService
     {
-        private static async Task ExecuteSendRecoveryEmail(string email, string name)
+        private static readonly string RecoveryUrl = "https://true-lime.herokuapp.com/updatepassword/";
+
+        public static async Task ExecuteSendRecoveryEmail(string email, string token)
         {
             var apiKey = ConfigConstants.SendGridKey;
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress(ConfigConstants.ServiceEmail, ConfigConstants.ServiceName);
             var subject = ConfigConstants.Subject;
-            var to = new EmailAddress(email, name);
-            var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var to = new EmailAddress(email);
+            var plainTextContent = ConfigConstants.DescriptionDutch + GetRecoveryUrl(token);
+            var htmlContent = $"<strong>{ConfigConstants.DescriptionDutch + GetRecoveryUrl(token)}</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
+        }
+
+        private static string GetRecoveryUrl(string token)
+        {
+            return RecoveryUrl + token;
         }
     }
 }
